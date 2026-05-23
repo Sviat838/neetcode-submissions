1d8275de0256e29ddef8e52d28d718e56cf82a63
Sviat838 <79668754+Sviat838@users.noreply.github.com>
2026-05-23 20:32:32 +0700

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,86 @@
+class Solution:
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for y in range(len(board)):
+            for x in range(len(board[0])):
+                num = board[y][x]
+
+                if num == '.':
+                    continue
+
+                if num in rows[y] or num in cols[x] or num in boxes[(y//3, x//3)]:
+                    return False
+
+                rows[y].add(num)
+                cols[x].add(num)
+                boxes[(y//3, x//3)].add(num)
+
+
+        return True 
+    def isValidSudoku2(self, board: List[List[str]]) -> bool:
+        x = 0
+        y = 0
+
+        if not self.is_unique(board):
+            return False
+
+        while y < 9 and x < 9:
+            new_part = list()
+
+            while y % 3 != 0 or y == 0:
+                new_part.extend(board[y][x:x + 3])
+                y += 1
+                if y % 3 == 0:
+                    if not self.is_unique_list(new_part):
+                        return False
+
+            if x > 8 and y < 8:
+                y += 3
+                x = 0
+            elif x < 8:
+                x += 3
+                y = 0
+
+        return True
+
+    def is_unique(self, part: List[List[str]]):
+
+        for y in range(len(part)):
+            x_temp = set()
+            x_dots = 0
+            y_temp = set()
+            y_dots = 0
+
+            for x in range(len(part[0])):
+                if part[y][x] == '.':
+                    x_dots += 1
+                else:
+                    x_temp.add(int(part[y][x]))
+
+                if part[x][y] == '.':
+                    y_dots += 1
+                else:
+                    y_temp.add(int(part[x][y]))
+
+            if len(x_temp) + x_dots != len(part[0]) or len(y_temp) + y_dots != len(part):
+                return False
+
+        return True
+
+    def is_unique_list(self, s):
+        dots = 0
+        res = set()
+
+        for e in s:
+            if e == '.':
+                dots += 1
+            else:
+                res.add(e)
+
+        return len(s) == dots + len(res)
+
+
